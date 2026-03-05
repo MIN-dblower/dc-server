fc379e403cf0ba6462b9a6869740410c238c29dc
Kurai <q93cxyjsvhazj7zzramb31pa6h3sxo@bots.bitbucket.org>
2026-03-05 21:18:48 +0000

    feat/DREAM-124: add a route for for file uploader

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,16 @@
 import app, { init } from './app';
 import { DataCenterScraper } from './services/datacenter';
+import { loadEnvConfig } from './config/env.config';
 
-async function scrapeDataCenter() {
-  const scraper = new DataCenterScraper();
-  const page = await scraper.openScraper();
-  // scraper.startTokenInvalidation(page, 1000 * 60 * 5);
-  // await scraper.loginAPI(page);
-  // const data = await scraper.getData(page);
-  // const result = scraper.anaylzeContent(data);
-  // console.log('HERE', JSON.stringify(result, null, 2));
-}
-// scrapeKBB();
-// scrapeJDPower();
-// scrapeDataCenter();
+// Load environment configuration once for the main HTTP server
+loadEnvConfig();
+
+
 init().then(async () => {
-  await scrapeDataCenter();
+  // await scrapeDataCenter();
   app.listen(10003, () => {
-    console.log('App successfully started.');
+    console.log(
+      'App successfully started. HTTP server listening on http://localhost:10003',
+    );
   });
 });
