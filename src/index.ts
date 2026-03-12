edf1c2882945a45d2a60a671830dbe5d0a825898
Kurai <q93cxyjsvhazj7zzramb31pa6h3sxo@bots.bitbucket.org>
2026-03-12 18:23:14 +0000

    feat/DREAM-124: update pm2 deployment script

--- src/index.ts
+++ src/index.ts
@@ -4,13 +4,18 @@ import { loadEnvConfig } from './config/env.config';
 
 // Load environment configuration once for the main HTTP server
 loadEnvConfig();
+const PORT = process.env.BULL_BOARD_PORT
+  ? parseInt(process.env.BULL_BOARD_PORT, 10)
+  : 3001;
+
 
 
 init().then(async () => {
   // await scrapeDataCenter();
-  app.listen(10003, () => {
+  
+  app.listen(PORT, () => {
     console.log(
-      'App successfully started. HTTP server listening on http://localhost:10003',
+      `App successfully started. HTTP server listening on http://localhost: ${PORT}`,
     );
   });
 });
